8f1edb81ebc68db42e98e8847d71bd7622a37d1e
yangmao <loveyangmao@126.com>
2018-09-09 19:21:44 +0800

    修改血糖测量提醒

--- app/Console/Commands/SendWeather.php
+++ app/Console/Commands/SendWeather.php
@@ -32,42 +32,40 @@ class SendWeather extends Command
     public function __construct()
     {
         parent::__construct();
-
     }
 
     /**
      * Execute the console command.
      *
-     * @return mixed
+     * @return void
      */
     public function handle()
     {
 
         $data = httpRequest('http://bj.weather.com.cn');
 
-        preg_match("/<h2 style=\"position\: relative\;\" class=\"weatheH1\">\s+医疗气象指数\((.*?)\)\s+<\/h2>/",$data,$result);//
+        preg_match("/<h2 style=\"position\: relative\;\" class=\"weatheH1\">\s+医疗气象指数\((.*?)\)\s+<\/h2>/", $data, $result);//
 
-        preg_match("/<section class=\"detail co\">\s+<aside>(.*?)<\/aside>\s+<a><\/a>\s+<\/section>/",$data,$say);
+        preg_match("/<section class=\"detail co\">\s+<aside>(.*?)<\/aside>\s+<a><\/a>\s+<\/section>/", $data, $say);
 
 
 
         $allergy = '';
-        if(!empty($result[1]))
-        {
-            $say[1] = str_replace(["<b>","</b>"],["",","],$say[1]);
+        if (!empty($result[1])) {
+            $say[1] = str_replace(["<b>","</b>"], ["",","], $say[1]);
+
             $allergy = '指数:'.$say[1].",".trim($result[1]);
-            $allergy = str_replace([",","，"],"\n",$allergy);
+
+            $allergy = str_replace([",","，"], "\n", $allergy);
 
         }
 
-        if(!empty($allergy))
-        {
+        if (!empty($allergy)) {
             $app = app('wechat.official_account');
 
             $app->broadcasting->previewText($allergy, 'oD5CP0vCCbDNx1dAdfWEA-yYU7MQ');
+
             $app->broadcasting->previewText($allergy, 'oD5CP0k-zzGQ8eLNQrlcaWCZP-n0');
         }
-
-
     }
 }

ac10f6121a26672cef8becbff8d0cd377d00417b
yangmao <loveyangmao@126.com>
2018-09-28 18:52:41 +0800

    修改花粉过敏提醒

--- weiboLogin.py
+++ weiboLogin.py
@@ -41,6 +41,7 @@ class WeiboLogin(object):
         self.session = requests.Session()
         self.cookie_path = cookie_path
         self.session.cookies = cookielib.LWPCookieJar(filename=self.cookie_path)
+        print(self.session.cookies)
         self.index_url = "http://weibo.com/login.php"
         self.session.get(self.index_url, headers=headers, timeout=2)
         self.postdata = dict()

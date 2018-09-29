b21866a20d0e655d546b7c960c998e41863d7de9
yangmao <loveyangmao@126.com>
2018-09-29 17:06:59 +0800

    修改花粉过敏提醒

--- app/Console/Commands/SendWeather.php
+++ app/Console/Commands/SendWeather.php
@@ -43,19 +43,21 @@ class SendWeather extends Command
     {
 
         $fileName = str_replace("app/Console/Commands", "", trim(dirname(__FILE__)))."public/data/pollen.data";
-        $data = file_get_contents($fileName);
-        if (!empty($data)) {
-            $allergy = str_replace(["#"], "\n", $data);
-        }
+        if (file_exists($fileName)) {
+            $data = file_get_contents($fileName);
+            if (!empty($data)) {
+                $allergy = str_replace(["#"], "\n", $data);
+            }
 
-        if (!empty($allergy)) {
-            $app = app('wechat.official_account');
+            if (!empty($allergy)) {
+                $app = app('wechat.official_account');
 
-            $app->broadcasting->previewText($allergy, 'oD5CP0vCCbDNx1dAdfWEA-yYU7MQ');
+                $app->broadcasting->previewText($allergy, 'oD5CP0vCCbDNx1dAdfWEA-yYU7MQ');
 
-            $app->broadcasting->previewText($allergy, 'oD5CP0k-zzGQ8eLNQrlcaWCZP-n0');
+                $app->broadcasting->previewText($allergy, 'oD5CP0k-zzGQ8eLNQrlcaWCZP-n0');
 
-            @unlink($fileName);
+                @unlink($fileName);
+            }
         }
     }
 }
